59f9895e2f0be56c3403730b868c56b4c7c1b545
dwlhm <dwiilhamm026@gmail.com>
2024-03-09 10:04:30 +0700

    setup flakes for dotfiles

---
+++ home.nix
@@ -0,0 +1,25 @@
+{pkgs, ...}: {
+    home.username = "dwlhm";
+    home.homeDirectory = "/home/dwlhm";
+    home.stateVersion = "24.05"; # To figure this out you can comment out the line and see what version it expected.
+
+    home.packages = [
+        pkgs.nerdfonts
+        pkgs.zoxide
+        pkgs.neovim
+	pkgs.lsd
+    ];
+
+    programs.home-manager.enable = true;
+
+    programs.fish = {
+        enable = true;
+	shellInit = ''
+	    ${pkgs.zoxide}/bin/zoxide init fish | source
+	'';
+	shellAliases = {
+	    ls = "lsd";
+	};
+    };
+
+}
