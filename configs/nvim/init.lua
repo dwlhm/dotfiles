ab282bafe18a2de36b58983159e7a13f022a25e7
dwlhm <dwiilhamm026@gmail.com>
2024-03-12 15:07:08 +0700

    use nvchad custom nvim-tree keymap

--- configs/nvim/init.lua
+++ configs/nvim/init.lua
@@ -7,4 +7,10 @@ vim.g.flavour = 'mocha'
 
 require('lualine').setup()
 
-require('nvim-tree-lua').setup()
+require('nvim-tree').setup()
+
+local map = vim.keymap.set
+
+-- nvimtree
+map("n", "<C-n>", "<cmd>NvimTreeToggle<CR>", { desc = "Nvimtree Toggle window" })
+map("n", "<leader>e", "<cmd>NvimTreeFocus<CR>", { desc = "Nvimtree Focus window" })
