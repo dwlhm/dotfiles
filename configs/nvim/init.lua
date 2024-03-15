5d59c2d8eee3090cbd5c581f4e1873136fdd7279
dwlhm <dwiilhamm026@gmail.com>
2024-03-15 11:33:48 +0700

    setup nvim-cmp

--- configs/nvim/init.lua
+++ configs/nvim/init.lua
@@ -23,3 +23,101 @@ require("mason-lspconfig").setup({
 		"tsserver",
 	}
 })
+
+local opts = { noremap=true, silent=true }
+
+-- Basic diagnostic mappings, these will navigate to or display diagnostics
+map('n', '<space>d', vim.diagnostic.open_float, opts)
+map('n', '[d', vim.diagnostic.goto_prev, opts)
+map('n', ']d', vim.diagnostic.goto_next, opts)
+map('n', '<space>q', vim.diagnostic.setloclist, opts)
+
+local on_attach = function(client, bufnr)
+
+  -- Enable completion triggered by <c-x><c-o>
+  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+  -- Mappings to magical LSP functions!
+  local bufopts = { noremap=true, silent=true, buffer=bufnr }
+  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+
+  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+  vim.keymap.set('n', 'gk', vim.lsp.buf.hover, bufopts)
+  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+  vim.keymap.set('n', 'gK', vim.lsp.buf.signature_help, bufopts)
+  vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
+  vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
+  vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
+  vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+  vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, bufopts)
+end
+
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+--for visualstudio lsps
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+--activate lsps
+local lspconfig = require("lspconfig")
+
+require("mason-lspconfig").setup_handlers {
+	-- default handler
+	function (server_name) 
+		require("lspconfig")[server_name].setup {
+			on_attach = on_attach,
+			capabilities = capabilities,
+		}
+	end,
+
+	-- dedicated handler start here
+}
+
+-- CMP - Autocompletion
+local cmp = require "cmp"
+cmp.setup {
+	mapping = {
+		['<C-p>'] = cmp.mapping.select_prev_item(),
+    ['<C-n>'] = cmp.mapping.select_next_item(),
+    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.close(),
+
+    ['<CR>'] = cmp.mapping.confirm {
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = true,
+    },
+
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      elseif has_words_before() then
+        cmp.complete()
+
+      else
+        fallback()
+      end
+
+    end, { "i", "s" }),
+
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
+
+      if cmp.visible() then
+
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+  },
+  sources = {
+    { name = 'nvim_lsp' },
+    { name = 'nvim_lsp_signature_help' },
+
+    { name = 'buffer' },
+    { name = 'path' }
+  },
